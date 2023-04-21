7e83f8fa5d583b6c0c891dc272329b2e391360da
AIA-Ubuntu <aiaindustrial@uniandes.edu.co>
2023-04-21 12:21:27 -0500

    Actualiza moveit_python_interface

---
+++ CeSiM-AI-Proyectos/control-brazo/moveit_pyhon_interface.py
@@ -0,0 +1,61 @@
+from __future__ import print_function
+from six.moves import input
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+try:
+    from math import pi, tau, dist, fabs, cos
+except:  # For Python 2 compatibility
+    from math import pi, fabs, cos, sqrt
+
+    tau = 2.0 * pi
+
+    def dist(p, q):
+        return sqrt(sum((p_i - q_i) ** 2.0 for p_i, q_i in zip(p, q)))
+
+
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node("move_group_python_interface", anonymous=True)
+
+robot = moveit_commander.RobotCommander()
+
+scene = moveit_commander.PlanningSceneInterface()
+
+group_name = "ur3"
+move_group = moveit_commander.MoveGroupCommander(group_name)
+
+display_trajectory_publisher = rospy.Publisher(
+    "/move_group/display_planned_path",
+    moveit_msgs.msg.DisplayTrajectory,
+    queue_size=20,
+)
+
+
+
+# We can get the name of the reference frame for this robot:
+planning_frame = move_group.get_planning_frame()
+print("============ Planning frame: %s" % planning_frame)
+
+# We can also print the name of the end-effector link for this group:
+eef_link = move_group.get_end_effector_link()
+print("============ End effector link: %s" % eef_link)
+
+# We can get a list of all the groups in the robot:
+group_names = robot.get_group_names()
+print("============ Available Planning Groups:", robot.get_group_names())
+
+# Sometimes for debugging it is useful to print the entire state of the
+# robot:
+print("============ Printing robot state")
+print(robot.get_current_state())
+print("")
+
+
