78f18d9d34a3a5dabe699b71a282800eebd60588
AIA-Ubuntu <aiaindustrial@uniandes.edu.co>
2023-04-24 09:49:31 -0500

    Instala entorno de braso en el ws

--- install/base_install.sh
+++ install/base_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd ~/catkin_ws/src
 catkin_create_pkg moveit_python_interface std_msgs rospy roscpp
-
+mkdir ~/catkin_ws/src/moveit_python_interface/scripts
+cp ~/CeSiM-AI/CeSiM-AI-Proyectos/control-brazo/moveit_pyhon_interface.py ~/catkin_ws/src/moveit_python_interface/scripts
+cd ~/catkin_ws
+catkin_make
