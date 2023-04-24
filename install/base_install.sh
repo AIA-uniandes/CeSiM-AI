5540ff44091e42447dcaa5f912575d3cfe8dc9c7
AIA-Ubuntu <aiaindustrial@uniandes.edu.co>
2023-04-24 15:51:57 -0500

    Corrrige base_install

--- install/base_install.sh
+++ install/base_install.sh
@@ -2,6 +2,6 @@
 cd ~/catkin_ws/src
 catkin_create_pkg moveit_python_interface std_msgs rospy roscpp
 mkdir ~/catkin_ws/src/moveit_python_interface/scripts
-cp ~/CeSiM-AI/CeSiM-AI-Proyectos/control-brazo/moveit_pyhon_interface.py ~/catkin_ws/src/moveit_python_interface/scripts
+cp ~/CeSiM-AI/CeSiM-AI-Proyectos/control-brazo/moveit_python_interface.py ~/catkin_ws/src/moveit_python_interface/scripts
 cd ~/catkin_ws
 catkin_make
