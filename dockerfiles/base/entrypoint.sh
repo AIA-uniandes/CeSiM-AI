69fe6f9a4b3bd2fc656a857e9cf59243b469498d
AIA-Ubuntu <aiaindustrial@uniandes.edu.co>
2023-04-26 12:17:53 -0500

    Cambia install por setup para mejorar claridad

--- dockerfiles/base/entrypoint.sh
+++ dockerfiles/base/entrypoint.sh
@@ -3,10 +3,10 @@ set -eu
 
 git clone https://github.com/AIA-uniandes/CeSiM-AI.git
 
-if [ -f "./CeSiM-AI/install/base_install.sh" ]; then
-    ./CeSiM-AI/install/base_install.sh
+if [ -f "./CeSiM-AI/setup/base_setup.sh" ]; then
+    ./CeSiM-AI/setup/base_setup.sh
 else
-    echo "File not found: ./CeSiM-AI/install/base_install.sh"
+    echo "File not found: ./CeSiM-AI/setup/base_setup.sh"
 fi
 
 exec "$@"
